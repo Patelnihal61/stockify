8eba414d8c8f6b4884a98bec1b5d5f4a755c8d64
Patelnihal61 <nihalnpatel@gmail.com>
2024-06-17 20:35:32 +0530

    initial commit

---
+++ src/components/Footer.js
@@ -0,0 +1,40 @@
+import React, { useState, useEffect } from "react";
+
+function Footer() {
+  // Scroll State
+  const [isVisible, setIsVisible] = useState(false);
+  const scrollTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  };
+
+  const listenToScroll = () => {
+    let heightToHidden = 250;
+    const windowScroll =
+      document.body.scrollTop || document.documentElement.scrollTop;
+
+    windowScroll > heightToHidden ? setIsVisible(true) : setIsVisible(false);
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", listenToScroll);
+  });
+  return (
+    <>
+      <footer>
+      <h1>this is footer</h1>
+      </footer>
+
+      {/* Sroll To Top */}
+      {isVisible && (
+        <div className="scroll_top" onClick={scrollTop}>
+          <i class="bi bi-arrow-up"></i>
+        </div>
+      )}
+    </>
+  );
+}
+
+export default Footer;
---
+++ src/pages/Home/Home.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Layout from '../../components/Layout'
+import Section1 from './Section1'
+
+export default function Home() {
+  return (
+    <>
+      <Layout>
+        <Section1/>
+      </Layout>
+    </>
+  )
+}
